8b3ad7882168689225879043b19d8bf7f2519e27
Ken Eucker <keneucker@gmail.com>
2017-01-12 13:58:54 -0800

    Added subheadings to each section and made more content changes.

--- templates/simply-wonder-theme/page-directive.js
+++ templates/simply-wonder-theme/page-directive.js
@@ -2,18 +2,15 @@ window.page.directive = {
     '#heading': 'heading',
     '.slides section': {
         'splash<-splashes':{
-            '#heading':'splash.heading',
+            '.heading':'splash.heading',
             'img.delay1':'splash.img1',
             'img.delay2':'splash.img2',
             'img.delay3':'splash.img3'
         }
     },
-    '#about-title': 'about-title',
-    '#about-link': 'about-title',
-    '#about-blurb .original > h3': 'about-blurb-heading',
-    '#about-blurb .original > p': 'about-blurb',
     '#portfolio-link': 'portfolio-heading',
     '#portfolio .section-title': 'portfolio-heading',
+    '#portfolio .section-description': 'portfolio-description',
     '#portfolio .gallery-item': {
         'video<-videos':{
             'a@href': 'video.url',
@@ -22,13 +19,19 @@ window.page.directive = {
             'p > i': 'video.subtitle'
         }
     },
-    '#mentions .section-title': 'Mentioned In',
+    '#mentions .section-title': 'mentions-heading',
+    '#mentions .section-description': 'mentions-description',
     '#mentions  div.column': {
         'mention<-mentions':{
             'img@alt': 'mention.alt',
             'img@src': 'mention.img'
         }
     },
+    '#about .section-title': 'about-heading',
+    '#about .section-description': 'about-description',
+    '#about-link': 'about-title',
+    '#about-blurb .original > h3': 'about-blurb-heading',
+    '#about-blurb .original > p': 'about-blurb',
     '#team  div.rows': {
         'member<-members':{
             '.our-team-img img@src': 'member.img',
@@ -41,6 +44,8 @@ window.page.directive = {
             // '.pinterest@href': 'member.pinterest',
         }
     },
+    '#events .section-title': 'events-heading',
+    '#events .section-description': 'events-description',
     "#events .event": {
         'event<-events': {
             '.img@src': 'event.image',
