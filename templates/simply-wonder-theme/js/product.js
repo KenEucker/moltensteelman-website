eb7047ab9efbfab01439ddfa42b3f780920362c6
Ken Eucker <keneucker@gmail.com>
2017-01-29 16:36:11 -0800

    Fixed a bug with the paypal integration not beig present.

--- templates/simply-wonder-theme/js/product.js
+++ templates/simply-wonder-theme/js/product.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
     // Paypal translate html form
     var paypalFormTable = $('form[action="https://www.paypal.com/cgi-bin/webscr"] table'), 
-        paypalFormSubmitButton = $('form[action="https://www.paypal.com/cgi-bin/webscr"] input[name="submit"]'),
-        replaceHtml = paypalFormTable.html().replace('<tr><td>','').replace('</td></tr>','').replace('<tbody>','').replace('</tbody>',''),
-        paymentPrice = $('#description .price'),
-        paypalPriceSelector = $('select[name="os0"]'),
-        paypalPriceSelectorCurrentValue = paypalPriceSelector.find('option:selected').text(),
-        papalPriceCurrentPrice = paypalPriceSelectorCurrentValue.substr(paypalPriceSelectorCurrentValue.indexOf('$') - 1);
+        paypalFormSubmitButton = $('form[action="https://www.paypal.com/cgi-bin/webscr"] input[name="submit"]');
 
-        paypalFormTable.replaceWith(replaceHtml);
-        paypalFormSubmitButton.addClass('button').addClass('large').addClass('expanded').attr('src','').attr('type','submit').attr('value','Place Order');
-        paymentPrice.html(papalPriceCurrentPrice);
-        $(document).on('change', 'select[name="os0"]', function(e){
-            var option = this.options[e.target.selectedIndex].text;
-            paymentPrice.html(option.substr(option.indexOf('$') - 1));
-        });
+        if(paypalFormTable.length) {
+            var 
+            paymentPrice = $('#description .price'),
+            paypalPriceSelector = $('select[name="os0"]'),
+            paypalPriceSelectorCurrentValue = paypalPriceSelector.find('option:selected').text(),
+            papalPriceCurrentPrice = paypalPriceSelectorCurrentValue.substr(paypalPriceSelectorCurrentValue.indexOf('$') - 1);
+
+            paypalFormTable.replaceWith(paypalFormTable.html().replace('<tr><td>','').replace('</td></tr>','').replace('<tbody>','').replace('</tbody>',''));
+            paypalFormSubmitButton.addClass('button').addClass('large').addClass('expanded').attr('src','').attr('type','submit').attr('value','Place Order');
+            paymentPrice.html(papalPriceCurrentPrice);
+            $(document).on('change', 'select[name="os0"]', function(e){
+                var option = this.options[e.target.selectedIndex].text;
+                paymentPrice.html(option.substr(option.indexOf('$') - 1));
+            });
+        }
     
     $('i.icon-youtube').hide();
 
